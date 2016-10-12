ba1651a18940d017c2156021d9296994f0b68229
ben slama <ben.slama.mohamed.ali@gmail.com>
2016-10-12 14:59:08 +0200

    Add Groups controller

---
+++ src/AppBundle/Controller/GroupsController.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace AppBundle\Controller;
+
+/**
+ * Description of GroupsController
+ *
+ * @author mas
+ */
+class GroupsController {
+    public function getGroupsAction()
+    {
+        
+    }
+    
+    public function getGroupAction($id)
+    {
+        
+    }
+    
+    public function putGroupsAction()
+    {
+        
+    }
+    
+    public function PostGroupsAction()
+    {
+        
+    }
+}
